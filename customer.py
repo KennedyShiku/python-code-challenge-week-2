5fbe9a8a1531c332878542db0e1e46cc0aebac89
KennedyShiku <kenrichshiku@gmail.com>
2023-12-18 11:44:45 +0300

    Added instantiation

---
+++ customer.py
@@ -0,0 +1,7 @@
+class Customer:
+    all_customers = []
+
+    def __init__(self, given_name, family_name):
+        self.given_name = given_name
+        self.family_name = family_name
+        Customer.all_customers.append(self)
